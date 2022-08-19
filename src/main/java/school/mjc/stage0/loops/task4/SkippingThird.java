d2483e619f0528d075f829640da2d473951ca798
kukumaj <zakumaj@gmail.com>
2022-08-19 23:47:17 +0200

    done

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -3,5 +3,16 @@ package school.mjc.stage0.loops.task4;
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
 
+        int counter = 0;
+        do {
+            if (counter % 3 == 0) {
+                counter++;
+                continue;
+            } else {
+                System.out.println(counter);
+            }
+            counter++;
+        } while (counter <= lastPrinted);
     }
 }
+
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,34 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
-
+        if (lastInRow < 0) {
+            System.out.println("last number in row is negative");
+        } else if (numberToSkip == lastInRow) {
+            int counter = 1;
+            int sum = 0;
+            do {
+                sum = sum + counter;
+                counter++;
+            } while (counter <= lastInRow);
+            System.out.println("skipped sum is " + sum);
+            System.out.println("counted sum is 0");
+        } else if (numberToSkip > lastInRow) {
+            System.out.println("number to skip is bugger then the last");
+        } else {
+            int counter = 1;
+            int sum = 0;
+            do {
+                sum = sum + counter;
+                counter++;
+            } while (counter <= lastInRow - numberToSkip);
+            System.out.println("skipped sum is " + sum);
+            int counter2 = numberToSkip + 1;
+            int sum2 = 0;
+            do {
+                sum2 = sum2 + counter2;
+                counter2++;
+            } while (counter2 <= lastInRow);
+            System.out.println("counted sum is " + sum2);
+        }
     }
 }
